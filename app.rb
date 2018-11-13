61c79c4dc0c7d46b49fbf4ca3349622c389e4f5d
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-11-13 11:04:05 +0100

    Initialize repo with Sinatra and Thin

---
+++ app.rb
@@ -0,0 +1,11 @@
+
+# require 'bundler'
+# Bundler.require(:default)
+# require 'sinatra/reloader'
+# require_relative 'GameOfLife.rb'
+
+require 'sinatra'
+
+get '/' do
+  "Hello world!"
+end
---
+++ game_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+describe 'game of life' do
+  it 'Follows the Rule 1: Any live cell with fewer than two live neighbors dies, as if by underpopulation.' do
+    # cell = Cell.new
+  end
+end
