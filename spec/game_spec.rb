bf04180a9b89f832233ac2ea39188c71c4992746
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-11-14 22:37:06 +0100

    Configure rspec

---
+++ spec/game_spec.rb
@@ -0,0 +1,14 @@
+require 'game'
+
+describe 'Game' do
+  describe 'atributes' do
+    # @size = size
+    # @current_board = Array.new(size) { Array.new(size, 0) }
+  end
+
+  describe 'populate_current_board' do
+  end
+
+  describe 'calculate_next_board' do
+  end
+end
